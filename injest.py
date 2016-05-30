df1653af3ee07eda0ed78ca88a535811d85b6cbe
jmmnn <jmmnn@yahoo.com>
2016-05-29 21:47:12 -0400

    Sample ingest function to solr
    new file

---
+++ injest.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+"""This program injests data into a Solr Server in schemaless mode.
+    """
+import solr  # More about this python package see: https://github.com/edsu/solrpy
+
+############# CONFIGURATION #############
+
+search_collection = 'new'     ##define the collection name
+search_server = 'http://server:port/solr/' + str(search_collection)
+
+#########################################
+
+### Injestion function ####
+  
+def injest ():
+    ###### Search server details 
+    s = solr.SolrConnection(search_server)  
+    
+    ###### Inject results to solr
+    try:
+        s.add ( Source = 'MySource', SDG = '17' , Sentence = 'hello world' , Type = 'article') # This is a sample doc
+        print 'Injested a doc'
+        s.commit()           
+    except:
+        print 'Error'
+        
+#Trigger the work
+injest()
